d4e4e265d70830849277995a70b9fcbd074c0758
fernandoceli <fernando_dfcv@hotmail.com>
2016-01-31 18:17:57 -0500

    Se agrega funcionalidad para listas en tablas de acuerdo a la aplicacion original

--- core/builder/controleshtml/ListaCuadriculada.class.php
+++ core/builder/controleshtml/ListaCuadriculada.class.php
@@ -4,66 +4,102 @@ require_once ("core/builder/HtmlBase.class.php");
 
 class ListaCuadriculada extends HtmlBase {
 	
-	function listaTabla($datos, $enlace) {
+	function listaTabla($atributos) {
+		
 		$this->cadenaHTML = "";
-
-		$this->setAtributos($datos);
 		
-		$this->campoSeguro();
+		$this->setAtributos ( $atributos );
 		
-		$this->cadenaHTML = array('');
+		$this->campoSeguro ();
 		
-		for($i = 0; $i < count ( $datos ); $i ++) {
-			$this->cadenaHTML[0] = '<div class="bg-caja corner" id="caja<?php ' . $i . ' ?>" style="float: left">';
-			$this->cadenaHTML[0] .= '<div class="caja corner">';
-			$this->cadenaHTML[0] .= '<div class="caja-header">';
-			$this->cadenaHTML[0] .= '<div class="caja-fecha" style="float: left">' . $datos[$i]['fecha'] . '</div>';
-			$this->cadenaHTML[0] .= '<div class="clearboth">';
-			$this->cadenaHTML[0] .= '<br></br>';
-			$this->cadenaHTML[0] .= '</div>';
-			$this->cadenaHTML[0] .= '</div>';
-			$this->cadenaHTML[0] .= '<div>';
-			$this->cadenaHTML[0] .= '<div class="caja-codigo" style="float: left">';
-			$this->cadenaHTML[0] .= '<div class="caja-icon-documento"></div>';
-			$this->cadenaHTML[0] .= '<p class="caja-numero" id="cajanum<?php echo $i ?>">No. ' . $datos[$i]['anteproyecto'] . '</p>';
-			$this->cadenaHTML[0] .= '</div>';
-			$this->cadenaHTML[0] .= '<div class="caja-info" style="float: left">';
-			$this->cadenaHTML[0] .= '<table style="border: 0; width: 100%">';
-			$this->cadenaHTML[0] .= '<tbody>';
-			$this->cadenaHTML[0] .= '<tr>';
-			$this->cadenaHTML[0] .= '<td><b>Titulo:</b></td>';
-			$this->cadenaHTML[0] .= '<td>' . $datos[$i]['titulo'] . '</td>';
-			$this->cadenaHTML[0] .= '</tr>';
-			$this->cadenaHTML[0] .= '<tr>';
-			$this->cadenaHTML[0] .= '<td><b>Modalidad:</b></td>';
-			$this->cadenaHTML[0] .= '<td>' . $datos[$i]['modalidad'] . '</td>';
-			$this->cadenaHTML[0] .= '</tr>';
-			$this->cadenaHTML[0] .= '<tr>';
-			$this->cadenaHTML[0] .= '<td><b>Estado:</b></td>';
-			$this->cadenaHTML[0] .= '<td>' . $datos[$i]['estado'] . '</td>';
-			$this->cadenaHTML[0] .= '</tr>';
-			$this->cadenaHTML[0] .= '</tbody>';
-			$this->cadenaHTML[0] .= '</table>';
-			$this->cadenaHTML[0] .= '</div>';
-			
-			$variableVer = "pagina=verAnteproyecto";
-			$variableVer .= "&usuario=" . $_REQUEST ['usuario'];
-			$variableVer .= "&anteproyecto=" . $datos [$i] ['anteproyecto'];
-			if (isset ( $docente )) {
-				$variableVer .= "&docente=" . $docente;
+		$this->cadenaHTML = array ('');
+		
+		if (isset ( $this->atributos ['matrizItems'] )) {
+			for($i = 0; $i < count ( $this->atributos ['matrizItems'] ); $i ++) {
+				$this->cadenaHTML [$i] = "<div ";
+				
+				if (isset ( $this->atributos ['clase'] )) {
+					$this->cadenaHTML [$i] .= "class='" . $this->atributos ['clase'] [0] . "' ";
+					$this->cadenaHTML [$i] .= "id='caja" . $i . "' ";
+					
+					if (isset ( $this->atributos ['estilo'] )) {
+						$this->cadenaHTML [$i] .= "style='" . $this->atributos ['estilo'] . "'>";
+					}
+				}
+				
+				if (isset ( $this->atributos ['clase'] )) {
+					$this->cadenaHTML [$i] .= "<div class='" . $this->atributos ['clase'] [1] . "'>";
+					$this->cadenaHTML [$i] .= "<div class='" . $this->atributos ['clase'] [2] . "'>";
+					$this->cadenaHTML [$i] .= "<div class='" . $this->atributos ['clase'] [3] . "' ";
+					if (isset ( $this->atributos ['estilo'] )) {
+						$this->cadenaHTML [$i] .= "style='" . $this->atributos ['estilo'] . "'>";
+					}
+				}
+				
+				$this->cadenaHTML [$i] .= $this->atributos ['matrizItems'] [$i] ['fecha'] . "</div>";
+				
+				if (isset ( $this->atributos ['clase'] )) {
+					$this->cadenaHTML [$i] .= "<div class='" . $this->atributos ['clase'] [4] . "'>";
+					$this->cadenaHTML [$i] .= "<br></br>";
+				}
+				
+				for($c = 0; $c < 2; $c ++) {
+					$this->cadenaHTML [$i] .= "</div>";
+				}
+				
+				$this->cadenaHTML [$i] .= "<div>";
+				
+				if (isset ( $this->atributos ['clase'] )) {
+					$this->cadenaHTML [$i] .= "<div class='" . $this->atributos ['clase'] [5] . "' ";
+					if (isset ( $this->atributos ['estilo'] )) {
+						$this->cadenaHTML [$i] .= "style='" . $this->atributos ['estilo'] . "'>";
+					}
+					$this->cadenaHTML [$i] .= "<div class='" . $this->atributos ['clase'] [6] . "'></div>";
+					$this->cadenaHTML [$i] .= "<p class='" . $this->atributos ['clase'] [7];
+					$this->cadenaHTML [$i] .= "id='cajanum" . $i . "'>";
+					if (isset ( $this->atributos ['matrizItems'] [$i] ['anteproyecto'] )) {
+						$this->cadenaHTML [$i] .= "No. " . $this->atributos ['matrizItems'] [$i] ['anteproyecto'] . "</p>";
+					}
+					if (isset ( $this->atributos ['matrizItems'] [$i] ['proyecto'] )) {
+						$this->cadenaHTML [$i] .= "No. " . $this->atributos ['matrizItems'] [$i] ['proyecto'] . "</p>";
+					}
+				}
+				
+				$this->cadenaHTML [$i] .= "</div>";
+				
+				if (isset ( $this->atributos ['clase'] )) {
+					$this->cadenaHTML [$i] .= "<div class='" . $this->atributos ['clase'] [8] . "' ";
+					if (isset ( $this->atributos ['estilo'] )) {
+						$this->cadenaHTML [$i] .= "style='" . $this->atributos ['estilo'] . "'>";
+					}
+				}
+				
+				if (isset ( $this->atributos ['estilo-tabla'] )) {
+					$this->cadenaHTML [$i] .= "<table style='" . $this->atributos ['estilo-tabla'] . "'>";
+					$this->cadenaHTML [$i] .= "<tbody>";
+					for($j = 0; $j < count ( $this->atributos ['datos'] ); $j ++) {
+						$this->cadenaHTML [$i] .= "<tr>";
+						$this->cadenaHTML [$i] .= "<td><b>" . $this->atributos ['datos'] [$j] . "</b></td>";
+						$this->cadenaHTML [$i] .= "<td>" . $this->atributos ['matrizItems'] [$i] [$j] . "</td>";
+						$this->cadenaHTML [$i] .= "</tr>";
+					}
+					$this->cadenaHTML [$i] .= "</tbody>";
+					$this->cadenaHTML [$i] .= "</table>";
+				}
+				
+				$this->cadenaHTML [$i] .= "<p></p>";
+				$this->cadenaHTML [$i] .= "</div>";
+				
+				if (isset ( $this->atributos ['enlaces'] )) {
+					$this->cadenaHTML [$i] .= $this->atributos ['enlaces'] [$i];
+				}
+				
+				for($c = 0; $c < 3; $c ++) {
+					$this->cadenaHTML [$i] .= "</div>";
+				}
 			}
-			$variableVer .= "&rol=" . $rol;
-				
-			$variableVer = $this->miConfigurador->fabricaConexiones->crypto->codificar_url ( $variableVer, $directorio );
-				
-			
-			$this->cadenaHTML[0] .= $enlace;
-			$this->cadenaHTML[0] .= '</div>';
-			$this->cadenaHTML[0] .= '</div>';
-			$this->cadenaHTML[0] .= '</div>';
 		}
 		
-		return $this->cadenaHTML[0];
+		return $this->cadenaHTML;
 	}
-	
 }
